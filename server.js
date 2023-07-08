a8c92fe797e6d6da60fbffd10c9480ffe2807651
yops320 <liamdavidson4@gmail.com>
2023-07-08 21:21:57 +0930

    db prep

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 // Middleware
 app.use(cors());
@@ -8,12 +9,10 @@ app.use(express.json());
 
 // Routes
 const userRoutes = require('./routes/user');
-app.use('/user', userRoutes);
-
 const rosterRoutes = require('./routes/roster');
-app.use('/roster', rosterRoutes);
-
 const shiftsRoutes = require('./routes/shift');
+app.use('/user', userRoutes);
+app.use('/roster', rosterRoutes);
 app.use('/shift', shiftsRoutes);
 
 // Sample Route
@@ -21,12 +20,17 @@ app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
 
-// Start the server
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
-
-
-
-
+// Connect to MongoDB
+const MONGO_URI = 'mongodb://localhost:27017/mydatabase'; // Replace 'mydatabase' with the name of your database
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to local MongoDB');
+    // Start the server after successful MongoDB connection
+    const port = process.env.PORT || 3000;
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
