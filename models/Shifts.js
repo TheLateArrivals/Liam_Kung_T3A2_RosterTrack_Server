21edd3f7ae86736eacafd6e398d25338b3b03e6a
Liam D <94531542+yops320@users.noreply.github.com>
2023-07-25 14:45:08 +0930

    Update Shifts.js
    update comments

--- models/Shifts.js
+++ models/Shifts.js
@@ -1,32 +1,36 @@
-const mongoose = require("mongoose")
+// Import required module for the application
+const mongoose = require("mongoose");
 
+// Define a Mongoose schema for the Shift collection in MongoDB
 const ShiftSchema = new mongoose.Schema({
   day: {
     type: String,
-    required: true,
+    required: true, // The "day" field is required for every shift document
   },
   date: {
     type: Date,
-    required: true,
+    required: true, // The "date" field is required for every shift document
   },
   employee_id: {
     type: String,
-    required: true,
+    required: true, // The "employee_id" field is required for every shift document
   },
   startTime: {
     type: Date,
-    required: true,
+    required: true, // The "startTime" field is required for every shift document
   },
   endTime: {
     type: Date,
-    required: true,
+    required: true, // The "endTime" field is required for every shift document
   },
   location: {
     type: String,
-    required: true,
+    required: true, // The "location" field is required for every shift document
   },
-})
+});
 
-const Shift = mongoose.model("Shift", ShiftSchema)
+// Create a Mongoose model named 'Shift' based on the defined schema
+const Shift = mongoose.model("Shift", ShiftSchema);
 
-module.exports = Shift
+// Export the Shift model to be used in other modules
+module.exports = Shift;
