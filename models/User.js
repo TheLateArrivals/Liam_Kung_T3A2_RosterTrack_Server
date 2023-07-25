1d54ec107fdc38a5236ffe7d2002c55ab48f036b
Liam D <94531542+yops320@users.noreply.github.com>
2023-07-25 14:44:14 +0930

    Update User.js
    update comments

--- models/User.js
+++ models/User.js
@@ -1,26 +1,30 @@
+// Import required module for the application
 const mongoose = require('mongoose');
 
+// Define a Mongoose schema for the User collection in MongoDB
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
-    required: true,
+    required: true, // The "username" field is required for every user document
   },
   password: {
     type: String,
-    required: true,
+    required: true, // The "password" field is required for every user document
   },
   email: {
     type: String,
-    required: true,
-    unique: true,
+    required: true, // The "email" field is required for every user document
+    unique: true, // Ensure that each email is unique in the User collection
   },
   is_admin: {
     type: Boolean,
-    default: false,
-    required: true,
+    default: false, // The "is_admin" field is a Boolean, and its default value is set to false if not specified
+    required: true, // The "is_admin" field is required for every user document
   }
 });
 
+// Create a Mongoose model named 'User' based on the defined schema
 const User = mongoose.model('User', userSchema);
 
-module.exports = User
+// Export the User model to be used in other modules
+module.exports = User;
