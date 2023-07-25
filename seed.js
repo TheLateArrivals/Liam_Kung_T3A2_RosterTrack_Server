cb3d600781bd135ba03a39deeedc56301294e276
Liam D <94531542+yops320@users.noreply.github.com>
2023-07-25 14:39:37 +0930

    Update seed.js
    Comments added

--- seed.js
+++ seed.js
@@ -5,23 +5,23 @@ require("dotenv").config()
 const User = require("./models/User")
 const Shift = require("./models/Shifts")
 
-
+// Create a user with a hashed password and store it in the database
 async function createUser(username, isAdmin) {
-  const hashedPassword = await bcrypt.hash("password", 10)
-  const user = await User.create({
+  const hashedPassword = await bcrypt.hash("password", 10) // bcrypt with cost factor 10
+  const user = await User.create({ // Create a new user in the database with the provided username, hashed password, and admin status
     username: username,
     password: hashedPassword,
     is_admin: isAdmin,
   })
   return user
 }
-
+// Connect to the MongoDB database
 mongoose.connect(process.env.MONGO_URI_TEST, async () => {
-  await User.deleteMany({})
-  const user = await createUser("user")
-  await createUser("admin", true)
-  await Shift.deleteMany({})
-  const shift = await Shift.create({
+  await User.deleteMany({}) // Delete all existing documents from the User collection
+  const user = await createUser("user")   // Create a regular user with the username "user"
+  await createUser("admin", true) // Create an admin user with the username "admin"
+  await Shift.deleteMany({}) // Delete all existing documents from the Shift collection
+  const shift = await Shift.create({ // Create a new shift entry in the Shift collection with the provided data
     day: "Monday",
     date: "2023-08-28",
     employee_id: user._id,
